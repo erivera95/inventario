0b62c6a40d711da428128e1629925bbee124a38b
Eduardo Rivera <erivera95@outlook.com>
2023-05-16 18:57:14 -0600

    Detallado tala menu equipos

--- portal/src/containers/equipomenu/equipo.js
+++ portal/src/containers/equipomenu/equipo.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState } from "react";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
 
-const EquipoContainer = () => {
+const EquipoContainer = (props) => {
+  const [products, setProducts] = useState([
+    { id: "0", Empresa: "tata", ciudad: "GDL", FechaDeCaptura: "16/05/2023" },
+    { id: "1", Empresa: "TCS", ciudad: "CDMX", FechaDeCaptura: "16/05/2023" },
+    { id: "2", Empresa: "USAA", ciudad: "MTY", FechaDeCaptura: "16/05/2023" },
+  ]);
   return (
-    <div>equipo menu</div>
-  )
-}
+    <div>
+      <DataTable
+        value={products}
+        paginator
+        rows={5}
+        tableStyle={{ minWidth: "50rem", maxHeight: "40rem" }}
+        stripedRows
+        removableSort
+      >
+        <Column field="id" header="Code" sortable></Column>
+        <Column field="Empresa" header="Name" sortable></Column>
+        <Column field="ciudad" header="Category" sortable></Column>
+        <Column field="FechaDeCaptura" header="Quantity" sortable></Column>
+      </DataTable>
+    </div>
+  );
+};
 
-export default EquipoContainer
+export default EquipoContainer;

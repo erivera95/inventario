fbd0b97a873b3f43c77d4d79cf5a43ec71821638
Eduardo Rivera <erivera95@outlook.com>
2023-05-19 18:13:13 -0600

    Reestructuracion del proyecto con forms y listas

---
+++ portal/src/containers/licenciasForm/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import LicenciasFormContainer from './licenciasForm'
+
+function LicenciasForm() {
+  return (
+    <div>
+        <LicenciasFormContainer/>
+    </div>
+  )
+}
+
+export default LicenciasForm
--- portal/src/containers/menu/menu.js
+++ portal/src/containers/menu/menu.js
@@ -4,7 +4,7 @@ import { MegaMenu } from "primereact/megamenu";
 import { InputText } from "primereact/inputtext";
 import { TabView, TabPanel } from "primereact/tabview";
 import EquipoMenu from "../equipomenu/index";
-import Licencias from "../licencias";
+import LicenciasForm from "../licenciasForm";
 const MenuContainer = () => {
   const items = [
     {
@@ -134,19 +134,8 @@ const MenuContainer = () => {
           <TabPanel header="Equipo de computo">
             <EquipoMenu/>
           </TabPanel>
-          <TabPanel header="Impresoras">
-            <p className="m-0">
-              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-              quae ab illo inventore veritatis et quasi architecto beatae vitae
-              dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-              aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
-              eos qui ratione voluptatem sequi nesciunt. Consectetur, adipisci
-              velit, sed quia non numquam eius modi.
-            </p>
-          </TabPanel>
           <TabPanel header="Licencias Office">
-            <Licencias/>
+            <LicenciasForm/>
           </TabPanel>
         </TabView>
       </div>
